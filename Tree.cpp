cbee4b7eb1007e77aee0eb89e6fde4219fb54395
Kundan Kumar Sharma <146988764+kundankumarsharma9@users.noreply.github.com>
2023-10-09 18:20:59 +0530

    Create Tree.cpp

---
+++ Tree.cpp
@@ -0,0 +1,61 @@
+// C++ program for above approach
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Tree node
+struct Node {
+int key;
+struct Node *left, *right;
+};
+
+// Utility function to create a new node
+Node* newNode(int key)
+{
+Node* temp = new Node;
+temp->key = key;
+temp->left = temp->right = NULL;
+return (temp);
+}
+
+/*Function to find the height(depth) of the tree*/
+int height(Node* root)
+{
+
+// Initialising a variable to count the
+// height of tree
+queue<Node*> q;
+q.push(root);
+int height = 0;
+while (!q.empty()) {
+	int size = q.size();
+	for (int i = 0; i < size; i++) {
+	Node* temp = q.front();
+	q.pop();
+	if (temp->left != NULL) {
+		q.push(temp->left);
+	}
+	if (temp->right != NULL) {
+		q.push(temp->right);
+	}
+	}
+	height++;
+}
+return height;
+}
+
+// Driver program
+int main()
+{
+
+// Let us create Binary Tree shown in above example
+Node* root = newNode(1);
+root->left = newNode(2);
+root->right = newNode(3);
+
+root->left->left = newNode(4);
+root->left->right = newNode(5);
+
+cout << "Height(Depth) of tree is: " << height(root);
+}
+
+// This code is contributed by Abhijeet Kumar(abhijeet19403)
